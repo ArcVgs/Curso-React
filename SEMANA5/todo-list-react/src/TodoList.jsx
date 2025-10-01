c38bfb66aa9e77ed4edbdc308cead67780b29727
jdelpozof <jcarlosdf492@gmail.com>
2025-09-30 20:12:11 -0500

    cuarto

---
+++ SEMANA5/todo-list-react/src/TodoList.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+
+//
+const  Task = ({task, index, onDelete, onComplete}) => {
+    return(
+        <div style={{textDecoration: task.completed ? 'line-through' : ''}}>
+            {task.title} 
+            <button style={{background: 'red'}} onClick={()=>{onDelete(index)}}>X</button>
+            <button style={{background: 'yellow'}} onClick={()=>{onComplete(index)}}>Complete</button>
+        </div>
+    )    
+}
+
+const CreateTask = ({addTask}) => {
+    const [inputValue, setInputValue] = useState('');
+    
+    const handleSubmit = (input) => {
+        input.preventDefault();
+        addTask(inputValue);
+        setInputValue('');        
+    };
+    
+    return(
+        <form onSubmit={handleSubmit}>
+             <input 
+             type="text"
+             placeholder='ADD new task'
+             value={inputValue}
+             onChange={(i)=> {setInputValue(i.target.value)}}
+             />           
+        </form>
+    )    
+}
+
+const TodoList = () => {
+    const [task, setTask] = useState([
+        { id:1, title: 'Task 1', completed: false},
+        { id:2, title: 'Task 2', completed: false},
+        { id:3, title: 'Task 3', completed: true}
+    ]);
+
+    const [taskCount, setTaskCount] = useState(0);
+    
+    useEffect(()=>{
+        setTaskCount(task.filter(task=> !task.completed).length)
+    },[task]);
+
+    const onDelete = (index) =>{
+        const newTask = [...task];
+        newTask.splice(index, 1);
+        setTask(newTask);
+    }
+    
+    const onComplete = (index) => {
+        const newTask = [...task];
+        newTask[index].completed = true 
+        setTask(newTask);        
+    }
+
+    const addTask = (title)=>{
+        const newTask = [...task, {title,completed: false}];
+        setTask(newTask)    
+    }
+  return (
+    <div className='todo-container'>
+        <h1>TODO LIST</h1>
+        <div>Tareas Pendientes {taskCount}</div>
+        <div>
+            {task.map((task, index ) => (
+                    <Task
+                    key={task.id}
+                    index={index}
+                    task = {task}                    
+                    onDelete = {onDelete}
+                    onComplete = {onComplete}                    
+                    />
+                )
+            )}            
+        </div>
+        <div>
+            <CreateTask addTask={addTask}/>      
+        </div>
+    </div>
+    
+  )
+}
+
+export default TodoList
---
+++ SEMANA5/todo-list-react/src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from 'react'
+import TodoList from './TodoList.jsx'
+import { createRoot } from 'react-dom/client'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <TodoList />
+  </StrictMode>,
+)
